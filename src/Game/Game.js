3f5c72d91b0fb2f915ec4440879bdd12c2d92f2f
Merwan Mahiout <60450166+Mahiout4@users.noreply.github.com>
2021-12-02 15:59:47 +0100

    addEquipeReponse

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -9,13 +9,41 @@ export default class Game extends Component {
                 <img src={Logo} alt="Blindify Logo"/>
                 <div className='background'>    
                     <div className= "gameContainer">
-                    <div className = "playlistName">
-                        <h2>Nom de la playlists</h2>
-                    </div>
-                    <div className= "question">
-                        <h2> Quelle est le nom de cette musique?</h2>
-                    </div>
-                    </div> 
+                        <div className = "playlistName">
+                            <h2 className ="namePlaylist">Nom de la playlists</h2>
+                        </div>
+                        <div className= "question">
+                            <h2 className="questionFont"> Quelle est le nom de cette musique?</h2>
+                        </div>
+                        <div className= "numberQuestion">
+                            <h2 className="fontNumber">1/15</h2>
+                        </div> 
+                           
+                    <div className = "equipeContainer">
+                        <div className = "equipe1" >
+                                <h3 className ="fontEquipe">Equipe 1</h3>
+                                <h4 className ="fontScore">1</h4>
+                        </div>
+                        <div className = "reponseContainer">
+                                <div className = "reponse1" >
+                                  <p className ="fontName">Artiste et Titre1</p>
+                                </div>
+                                <div className = "reponse2" >
+                                  <p className ="fontName">Artiste et Titre2</p>
+                                </div>
+                                <div className = "reponse2" >
+                                  <p className ="fontName">Artiste et Titre3</p>
+                                </div>
+                                <div className = "reponse4" >
+                                  <p className ="fontName">Artiste et Titre4</p>
+                                </div>
+                        </div>
+                            <div className = "equipe2" >
+                                <h3 className="fontEquipe">Equipe 2</h3>
+                                <h4 className ="fontScore">1</h4>
+                            </div> 
+                    </div>   
+                </div> 
                 </div>
           
             </div>
