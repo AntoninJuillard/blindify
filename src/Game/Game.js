4e5de47f86f2b9f7e427f1b9c911c8b20a35422f
Merwan Mahiout <60450166+Mahiout4@users.noreply.github.com>
2021-12-02 14:40:08 +0100

    WhiteBox

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import Logo from '../img/BlindifyLogo.png';
 import './Game.css';
 
 export default class Game extends Component {
     render() {
         return (
             <div>
-                <div className='background'></div>
+                <div className='background'>
                 <img src={Logo} alt="Blindify Logo"/>
+                <div className= "gameBox">
+                    <div className = "playlistName">
+                        <h2>Nom de la playlists</h2>
+                    </div>
+                    <div className= "Title">
+                        <h2> Quelle est le nom de cette musique?</h2>
+                    </div>
+                </div> 
+                </div>
+          
             </div>
         )
     }
