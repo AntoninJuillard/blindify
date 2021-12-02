59e729fb3ea727fcddb9d4d287f5a66d7117ce8b
Merwan Mahiout <60450166+Mahiout4@users.noreply.github.com>
2021-12-02 13:05:54 +0100

    Update Game.js

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Game extends Component {
     render() {
         return (
-            <div>
-                
+            <div className = "">
+            
             </div>
         )
     }
