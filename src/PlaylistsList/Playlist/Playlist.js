adc20cd5df57cc2c662afbb50377e3a32f88289f
AntoninJuillard <60604725+AntoninJuillard@users.noreply.github.com>
2021-12-02 13:45:37 +0100

    change

--- src/PlaylistsList/Playlist/Playlist.js
+++ src/PlaylistsList/Playlist/Playlist.js
@@ -4,7 +4,7 @@ import './Playlist.css'
 export default class Playlist extends Component {
     render() {
         return (
-            <div className="playlist" style={{background: red}}>
+            <div className="playlist" style={{background:'red'}}>
                 <div className="playlistHover">
                     <div className="triangle"></div>
                     <p>Lancer le blindtest</p>
