bc54a212129a087c17dc594fccb6047038691fd6
Sushi <misaiphonraphael@gmail.com>
2021-12-03 14:15:16 +0100

    add html

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -5,7 +5,44 @@ export default class Game extends Component {
     render() {
         return (
             <div>
-                le jeu
+                <img src={Logo} alt="Blindify Logo"/>
+                <div className='background'>    
+                    <div className= "gameContainer">
+                        <div className = "playlistName">
+                            <h2 className ="namePlaylist">Nom de la playlists</h2>
+                        </div>
+                        <div className= "question">
+                            <h2 className="questionFont"> Quelle est le nom de cette musique?</h2>
+                        </div>
+                        <div className= "numberQuestion">
+                            <h2 className="fontNumber">1/15</h2>
+                        </div>     
+                    <div className = "equipeContainer">
+                        <div className = "equipe1" >
+                                <h3 className ="fontEquipe">Equipe 1</h3>
+                                <h4>1</h4>
+                        </div>
+                        <div className = "responseContainer fontName">
+                                <div className = "response1" >
+                                  <p>Artiste et Titre1</p>
+                                </div>
+                                <div className = "response2" >
+                                  <p>Artiste et Titre2</p>
+                                </div>
+                                <div className = "response3" >
+                                  <p>Artiste et Titre3</p>
+                                </div>
+                                <div className = "response4" >
+                                  <p>Artiste et Titre4</p>
+                                </div>
+                        </div>
+                            <div className = "equipe2" >
+                                <h3 className="fontEquipe">Equipe 2</h3>
+                                <h4>1</h4>
+                            </div> 
+                    </div>   
+                </div> 
+                </div>
             </div>
         )
     }
