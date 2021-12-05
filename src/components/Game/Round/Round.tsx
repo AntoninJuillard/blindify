b9db87bc61dffa9c168ae8d0cbd0ea47ffacfc8a
AntoninJuillard <60604725+AntoninJuillard@users.noreply.github.com>
2021-12-05 21:47:36 +0100

    game update
    game.tsx is working !! :+1:

--- src/components/Game/Round/Round.tsx
+++ src/components/Game/Round/Round.tsx
@@ -5,39 +5,91 @@ export type roundProps = {
     tracksRound : string[],
     handleTracksRound: Function,
     tracks: string[]
+    handleScore: Function
 }
 
 export type roundState = {
     tracksRound: string[],
+    trueTrack: number,
+    chooseTeam: any
 }
 
-export default class Round extends Component <roundProps> {
+export default class Round extends Component <roundProps, roundState> {
     
-    handleRandomTracks = () => {
+    setRandomTracks = () => {
         let tracklistCopy = [...this.props.tracks]
         let tracksRound = [
-            tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1), 
-            tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1),
-            tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1),
-            tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1)
+            tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1)[0], 
+            tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1)[0],
+            tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1)[0],
+            tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1)[0]
         ]
-        console.log(this.props.tracks)
+        
         return tracksRound
     }
 
+    
     state = {
-        tracksRound: this.handleRandomTracks()
+        tracksRound: this.setRandomTracks(),
+        trueTrack: Math.floor(Math.random()*4),
+        chooseTeam: <section></section>
     }
 
-    
+    // generer 4 nouveaux morceaux aléatoires dans le state 
+    handleRandomTracks = () => {
+        let tracklistCopy = [...this.props.tracks]
+        let tracksRandom = {
+            tracksRound:[
+                tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1)[0], 
+                tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1)[0],
+                tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1)[0],
+                tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1)[0]
+            ],
+            trueTrack: Math.floor(Math.random()*4)
+        }
+        this.setState(tracksRandom)
+        console.log(this.state)
+       
+    }
+
+
+    handleChooseTeam = (team:number) => {
+        this.props.handleScore(team)
+        let state = {
+            tracksRound: this.state.tracksRound,
+            trueTrack: this.state.trueTrack,
+            chooseTeam: <section></section>
+        }
+        this.setState(state)
+    }
+
+    handleChooseTeamDisplay = () => {
+        let state = {
+            tracksRound: this.state.tracksRound,
+            trueTrack: this.state.trueTrack,
+            chooseTeam: <section className='teamChoice'><p>Qui a répondu ?</p><div onClick={() => {this.handleChooseTeam(1)}}>Equipe 1 </div><div onClick={() => {this.handleChooseTeam(2)}}>Equipe 2</div></section>
+        }
+        this.setState(state)
+    }
+
+    handleTrueTrack = (id:number) => {
+        if(id===this.state.trueTrack)
+        {
+            
+            this.handleRandomTracks()
+            this.handleChooseTeamDisplay()
+
+        }
+    }
     
     render() {
         return (
             <div className="buttonsContainer">
-                <div>{this.state.tracksRound[0]}</div>
-                <div>{this.state.tracksRound[1]}</div>
-                <div>{this.state.tracksRound[2]}</div>
-                <div>{this.state.tracksRound[3]}</div>
+                {this.state.chooseTeam}
+                <div onClick={() => {this.handleTrueTrack(0)}}>{this.state.tracksRound[0]}</div>
+                <div onClick={() => {this.handleTrueTrack(1)}}>{this.state.tracksRound[1]}</div>
+                <div onClick={() => {this.handleTrueTrack(2)}}>{this.state.tracksRound[2]}</div>
+                <div onClick={() => {this.handleTrueTrack(3)}}>{this.state.tracksRound[3]}</div>
             </div>
         )
     }
