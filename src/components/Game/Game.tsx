cb68b350cc575451ac90c68235aca6a61ddaefd5
AntoninJuillard <60604725+AntoninJuillard@users.noreply.github.com>
2021-12-05 20:25:51 +0100

    game update

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -22,32 +22,28 @@ export type gameState = {
 
 export default class Game extends Component <playlistProps, gameState>{
     
-    //handleTracklist : essayer de pouvoir changer le state et que les changements s'affiche dans le return 
-    state = {
-        tracks: ['tracksskssks ', ' hk;h', 'rer'],
-        tracksRound: [' bfdklj', ' ', ' ', ' '],
-        score1: 0,
-        score2: 0
-    }
 
-    // transformer la tracklist des props en string artist - name et stocker ca dans le state tracks
+    // transformer la tracklist des props en string artist - name 
     handleTracks = () => {
         let rawTrackList = this.props.playlist.tracks
         let trackList:string[] = []
         for(let i=0; i < rawTrackList.length; i++){
             trackList[i] = rawTrackList[i].artist + ' - ' + rawTrackList[i].trackname
         }
+        return trackList
 
-        let state = {
-            tracks: trackList,
-            tracksRound: this.state.tracksRound,
-            score1: this.state.score1,
-            score2: this.state.score2,
-        }
-        this.setState(state)
-
-        console.log(this.state.tracks)
+        
     }
+
+
+    state = {
+        tracks: this.handleTracks(),
+        tracksRound: [this.props.playlist.tracks[0].artist + ' - ' + this.props.playlist.tracks[0].trackname, this.props.playlist.tracks[1].artist + ' - ' + this.props.playlist.tracks[1].trackname, this.props.playlist.tracks[2].artist + ' - ' + this.props.playlist.tracks[2].trackname, this.props.playlist.tracks[3].artist + ' - ' + this.props.playlist.tracks[3].trackname],
+        score1: 0,
+        score2: 0
+    }
+
+    
 
 
 
@@ -117,7 +113,7 @@ export default class Game extends Component <playlistProps, gameState>{
                     <h2 className="playlistName">{this.props.playlist.name}</h2>
                     <div className="title" onClick={() => this.handleTracks()}>Quel est le nom de cette musique ?</div>
                     <div className="roundDisplay">/15</div>
-                    <Round  />
+                    <Round  tracks={this.state.tracks} tracksRound={this.state.tracksRound} handleTracksRound={this.handleTracksRound}/>
                     <div className='scoreDisplay scoreDisplayRight'>
                         <p className="scoreDisplayTitle">Equipe 2</p>  
                         <p>{this.state.score2}</p>
--- src/components/Game/Round/Round.tsx
+++ src/components/Game/Round/Round.tsx
@@ -1,21 +1,43 @@
 import React, { Component } from 'react'
 import './Round.css'
 
-// export type roundProps = {
-//     tracks : string[],
-//     tracksRound : string[],
-//     score1 : number, 
-//     score2 : number
-// }
+export type roundProps = {
+    tracksRound : string[],
+    handleTracksRound: Function,
+    tracks: string[]
+}
 
-export default class Round extends Component {
+export type roundState = {
+    tracksRound: string[],
+}
+
+export default class Round extends Component <roundProps> {
+    
+    handleRandomTracks = () => {
+        let tracklistCopy = [...this.props.tracks]
+        let tracksRound = [
+            tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1), 
+            tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1),
+            tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1),
+            tracklistCopy.splice(Math.floor(Math.random()*tracklistCopy.length),1)
+        ]
+        console.log(this.props.tracks)
+        return tracksRound
+    }
+
+    state = {
+        tracksRound: this.handleRandomTracks()
+    }
+
+    
+    
     render() {
         return (
             <div className="buttonsContainer">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>{this.state.tracksRound[0]}</div>
+                <div>{this.state.tracksRound[1]}</div>
+                <div>{this.state.tracksRound[2]}</div>
+                <div>{this.state.tracksRound[3]}</div>
             </div>
         )
     }
